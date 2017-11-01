4efe61d0c12ab5b92421f691deb1ce9262711b43
Nico Mollet <nico.mollet@gmail.com>
2017-11-01 16:23:20 +0100

    Update gift-wrap.php

--- templates/gift-wrap.php
+++ templates/gift-wrap.php
@@ -1,3 +1,12 @@
-<p class="gift-wrapping" style="clear:both; padding-top: .5em;">
+<?php
+/**
+ * @param $product WC_Product
+ * @param $price_text Price text
+ * @param $checkbox Checkbox value
+ * @param $product_gift_wrap_message Wrap message
+ */
+?>
+
+<p class="gift-wrapping gift-wrapping<?php echo ( $product->is_type( 'variation' ) ? '-variation' : '' ) ?>" id="gift-wrapping-variation-<?php echo $product->get_id()?>">
 	<label><?php echo str_replace( array( '{checkbox}', '{price}' ), array( $checkbox, $price_text ), wp_kses_post( $product_gift_wrap_message ) ); ?></label>
 </p>
